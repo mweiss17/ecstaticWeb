dcfae9ec51b410d0300f130c89214a4c7eb72502
martin <martin.weiss@mail.mcgill.ca>
2014-03-23 05:35:37 +0000

    changed stream page js/css

--- sds/templates/static/js/stream.js
+++ sds/templates/static/js/stream.js
@@ -19,16 +19,44 @@ this.playMix = function(){
 	}
 }
 
+var loadbutton = document.getElementById('load')
+var playButton = document.getElementById('play')
+var count = 0;
+var count2 = 0;
 var playtime = {{eta}};
+setInterval(showRemaining, 1000);
+loadbutton.addEventListener('click',hideshow,false);
+
+
 function showRemaining() {
 	document.getElementById('countdown').innerHTML = playtime;
 	playtime = playtime + 1;
+	if(playtime > -10){
+		makeLoadButtonVisible();
+	}
+	if(playtime >= 0){
+		makePlayButtonVisible();
+	}
+
 }
-setInterval(showRemaining, 1000);
+
+function makeLoadButtonVisible(){
+	if(count == 1){
+		return;
+	}
+	count++;
+	document.getElementById('load').style.display = 'block';
+}
+
+function makePlayButtonVisible(){
+	if(count2 == 1){
+		return;
+	}
+	count2++;
+	document.getElementById('play').style.display = 'block';
+}
 // hides the load button after you click it (if you reclick the load button, you move the mix forward by 10 millis)
-var loadbutton = document.getElementById('load')
-loadbutton.addEventListener('click',hideshow,false);
 function hideshow() {
     document.getElementById('load').style.display = 'block'; 
-    this.style.display = 'none'
-}   
+    document.getElementById('load').style.backgroundColor = '#B0B0B0'; 
+}  
