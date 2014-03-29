a36ac64289eb9e0c56531b6c69825c666bd9632d
martin <martin.weiss@mail.mcgill.ca>
2014-03-29 05:16:23 +0000

    fixed countdown timer on stream page

--- sds/templates/static/js/stream.js
+++ sds/templates/static/js/stream.js
@@ -1,19 +1,26 @@
 var mAudioPlayer = document.getElementsByTagName("audio")[0];
+var firstClick = true;
 this.loadMix = function(){
 	try{
 		this.mAudioPlayer.play();
-		setTimeout("mAudioPlayer.pause()", 10)
+		setTimeout("this.mAudioPlayer.pause()", 10)
 	}
 	catch(err){
 		
 console.log( err );
 	}
+	this.mAudioPlayer.pause();
 }
 
 this.playMix = function(){
 	try{
-		this.mAudioPlayer.currentTime = playtime;
+		console.log(playtime);
+		this.mAudioPlayer.currentTime = -playtime;
 		this.mAudioPlayer.play();
+		if(firstClick){
+			document.getElementById('play').innerHTML = "Re-Sync";
+			firstClick = false;
+		}
 	}
 	catch(err){
 		console.log( err );
@@ -30,15 +37,32 @@ loadbutton.addEventListener('click',hideshow,false);
 
 
 function showRemaining() {
-	document.getElementById('countdown').innerHTML = playtime;
-	playtime = playtime + 1;
-	if(playtime > -300){
+	playtime = playtime - 1;
+	if(playtime < 300){
 		makeLoadButtonVisible();
 	}
-	if(playtime >= 0){
+	if(playtime <= 0){
 		makePlayButtonVisible();
 	}
-
+	if(playtime <= 0){
+	var tempPlayTime = -playtime;
+	var hours = Math.floor(( tempPlayTime / 3600) % 24);
+	var minutes = Math.floor(( tempPlayTime / 60) % 60);
+	var seconds = Math.floor( tempPlayTime % 60);
+	document.getElementById("countdown").innerHTML = hours + 'hrs ';
+	document.getElementById("countdown").innerHTML += minutes + 'mins ';
+	document.getElementById("countdown").innerHTML += seconds + 'secs';
+	}
+	if(playtime > 0){
+	var days = Math.floor( playtime / 86400);
+	var hours = Math.floor(( playtime / 3600) % 24);
+	var minutes = Math.floor(( playtime / 60) % 60);
+	var seconds = Math.floor( playtime % 60);
+	document.getElementById("countdown").innerHTML = days + 'days ';
+	document.getElementById("countdown").innerHTML += hours + 'hrs ';
+	document.getElementById("countdown").innerHTML += minutes + 'mins ';
+	document.getElementById("countdown").innerHTML += seconds + 'secs';
+	}
 }
 
 function makeLoadButtonVisible(){
@@ -59,5 +83,5 @@ function makePlayButtonVisible(){
 // hides the load button after you click it (if you reclick the load button, you move the mix forward by 10 millis)
 function hideshow() {
     document.getElementById('load').style.display = 'none'; 
-    //document.getElementById('load').style.backgroundColor = '#B0B0B0'; 
 }  
+
