533bf295b7f2da53fe42a76be276bbe65c8a1a54
martin <martin.weiss@mail.mcgill.ca>
2014-05-10 14:09:20 +0000

    removed registration module

--- sds/urls.py
+++ sds/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', 'sds.views.logout'),
     url(r'^accounts/loggedin/$', 'sds.views.loggedin'),
     url(r'^accounts/invalid/$', 'sds.views.invalid_login'),
-    url(r'^accounts/', include('registration.urls')),    
+    url(r'^accounts/', include('allauth.urls')), 
     url(r'^admin/', include(admin.site.urls))
 
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
