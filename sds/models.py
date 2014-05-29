4091e41f75b157e99f15dd6a634522edb7078ce1
martin <martin.weiss@mail.mcgill.ca>
2014-05-29 20:23:25 +0000

    music model fix

--- sds/models.py
+++ sds/models.py
@@ -28,8 +28,7 @@ class Photos(models.Model):
 class Music(models.Model):
     uploadedSong = models.FileField(upload_to='uploadedSongs/%Y/%m/%d', default='uploadedSongs', blank=True)
     email = models.CharField(max_length=255)
-    songname = models.CharField(max_length=255)
-    song_name_or_link = models.CharField(max_length=255)
+    song_name_or_link = models.CharField(max_length=255, blank=True)
     intention = models.CharField(max_length=255, blank=True)
     musicUploadDate = models.DateTimeField("musicUploadDate", auto_now=True, blank=True, null=True)
 
