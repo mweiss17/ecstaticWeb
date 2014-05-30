3d3828a382c8f628eef77bb604f37e40bf098724
martin <martin.weiss@mail.mcgill.ca>
2014-05-30 17:25:30 +0000

    deployment upgrade

--- sds/fabfile.py
+++ sds/fabfile.py
@@ -5,17 +5,14 @@ env.key_filename = '~/.ssh/martin.pem'
 env.hosts = ['ec2-user@silentdiscosquad.com:22']
 
 def commit():
-    local("git add -p -A * && git commit")
+    local("git add -p && git commit")
 
 def prepare_deploy(branch_name):
     commit()
     local('git checkout master && git merge ' + branch_name)
-    local('git pull')
-    local('python manage.py migrate sds')
 
 def deploy():
     with cd('/home/ec2-user/sds'):
-    	local('git push')
         run('git pull')
         run('python manage.py migrate sds')
         run('sudo /etc/init.d/httpd restart')
