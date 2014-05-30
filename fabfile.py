b7edeec9e020ab04982ba51e43fc33be331144e4
martin <martin.weiss@mail.mcgill.ca>
2014-05-30 17:13:25 +0000

    fabfile changes

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,23 @@ from fabric.network import ssh
 env.key_filename = '~/.ssh/martin.pem'
 env.hosts = ['ec2-user@silentdiscosquad.com:22']
 
-def prepare_deployment(branch_name):
-    #local('python manage.py test sds')
-    local('git add -p && git commit') 
+def commit():
+    local("git add -p && git commit")
 
-def restart_webserver():
-    sudo('sudo /etc/init.d/httpd restart')
+def prepare_deploy(branch_name):
+    commit()
+    local('git checkout master && git merge ' + branch_name)
+    local('git pull')
+
+def deploy():
+    with cd('/home/ec2-user/sds'):
+    	local('git push')
+        run('git pull')
+        run('python manage.py migrate sds')
+        run('sudo /etc/init.d/httpd restart')
+
+def restart_live():
+    sudo('/etc/init.d/httpd restart')
+
+def restart_local():
+    local('sudo /etc/init.d/httpd restart')
