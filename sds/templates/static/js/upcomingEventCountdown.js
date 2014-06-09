3c6fbfa989b54c000b5f205f44b2c02f95a3c66c
martin <martin.weiss@mail.mcgill.ca>
2014-06-09 16:36:25 +0000

    added new index page that appears when there are no global discos lined up. Also working on redoing the admin section

--- sds/templates/static/js/upcomingEventCountdown.js
+++ sds/templates/static/js/upcomingEventCountdown.js
@@ -1,20 +1,23 @@
 function showRemaining() {
   for(i=0; i < secondsLeftArray.length; i++)
   {
-    if(future!='True' && secondsLeftArray[i] <= 0 && secondsLeftArray[i] >= -7200)
-    {
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML = "IN PROGRESS!";
-      continue;
+    try{
+      if(future!='True' && secondsLeftArray[i] <= 0 && secondsLeftArray[i] >= -7200)
+      {
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML = "IN PROGRESS!";
+        continue;
+      }
+        secondsLeftArray[i] = secondsLeftArray[i] - 1;
+        var days = Math.floor( secondsLeftArray[i] / 86400);
+        var hours = Math.floor( (secondsLeftArray[i] / 3600) % 24);
+        var minutes = Math.floor(( secondsLeftArray[i] / 60) % 60);
+        var seconds = Math.floor( secondsLeftArray[i] % 60);
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML = 'Disco Begins: ';
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += days + 'd ';
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += hours + 'h ';
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += minutes + 'm ';
+        document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += seconds + 's';
     }
-      secondsLeftArray[i] = secondsLeftArray[i] - 1;
-      var days = Math.floor( secondsLeftArray[i] / 86400);
-      var hours = Math.floor( (secondsLeftArray[i] / 3600) % 24);
-      var minutes = Math.floor(( secondsLeftArray[i] / 60) % 60);
-      var seconds = Math.floor( secondsLeftArray[i] % 60);
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML = 'Disco Begins: ';
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += days + 'days ';
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += hours + 'hrs ';
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += minutes + 'mins ';
-      document.getElementById("eventID"+upcomingEventsIDs[i]).innerHTML += seconds + 'secs';
+    catch(err){}
   }
 }
