0a0f44f76454230b5c270951ae1c2dcaf6326753
martin <martin.weiss@mail.mcgill.ca>
2014-06-10 15:03:48 +0000

    model change 1

--- fabfile.py
+++ fabfile.py
@@ -12,12 +12,13 @@ def prepare_deploy(branch_name):
         local('git checkout master && git merge ' + branch_name)
 
 def deploy():
-    with cd('/home/ec2-user/sds'):
-        local('git push')
-        run('git pull')
-        run('python manage.py schemamigration sds --auto')
-        run('python manage.py migrate sds')
-        run('sudo /etc/init.d/httpd restart')
+    with settings(warn_only=True):
+        with cd('/home/ec2-user/sds'):
+            local('git push')
+            run('git pull')
+            run('python manage.py schemamigration sds --auto')
+            run('python manage.py migrate sds')
+            run('sudo /etc/init.d/httpd restart')
 
 def reset_migrations():
     #navigate into the production database and run:  'drop table south_migrationhistory;'
