1455f7e784474258f51b91f1ba48bacaf0a2d0a0
martin <martin.weiss@mail.mcgill.ca>
2014-07-07 19:41:24 +0000

    abstracted global event mix

--- sds/models.py
+++ sds/models.py
@@ -44,6 +44,7 @@ class Music(models.Model):
 class globalEvent(models.Model):
     title = models.CharField(max_length=255)
     eventPic = models.ForeignKey(Photos, related_name="globalEventEventPic")
+    eventMix = models.ForeignKey(Music, blank=True, null=True, related_name="globalEventEventMix")
     indexHeaderPic = models.ForeignKey(Photos)
     arrive_start_time = models.DateTimeField("Event Start Time")
     music_start_time = models.DateTimeField("Music Start Time")
