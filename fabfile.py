87438c42c122f27909fb1ba7ccc18139f71840a0
martin <martin.weiss@mail.mcgill.ca>
2015-01-17 20:33:34 +0000

    fabric updated to handle autoloadbalancer

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,56 @@
-from fabric.api import *
+from fabric import api as fab
 from fabric.network import ssh
+import boto
 
-env.key_filename = '~/.ssh/martin.pem'
-env.hosts = ['ec2-user@54.173.246.101:22']
+LOAD_BALANCER_NAME = 'SDSLiveLoadBalancer'
+SERVER_USER = 'ec2-user'
+SSH_KEY_FILE = '~/.ssh/martin.pem'
+
+def aws_hosts():
+    # Get a list of instance IDs for the ELB.
+    instances = []
+    conn = boto.connect_elb()
+    for elb in conn.get_all_load_balancers(LOAD_BALANCER_NAME):
+        instances.extend(elb.instances)
+ 
+    # Get the instance IDs for the reservations.
+    conn = boto.connect_ec2()
+    reservations = conn.get_all_instances([i.id for i in instances])
+    instance_ids = []
+    for reservation in reservations:
+        for i in reservation.instances:
+            instance_ids.append(i.id)
+ 
+    # Get the public CNAMES for those instances.
+    hosts = []
+    for host in conn.get_all_instances(instance_ids):
+        hosts.extend([i.public_dns_name for i in host.instances])
+    hosts.sort() # Put them in a consistent order, so that calling code can do hosts[0] and hosts[1] consistently.
+ 
+    return hosts
+
+def aws():
+    fab.env.hosts = aws_hosts()
+    print fab.env.hosts
+    fab.env.key_filename = SSH_KEY_FILE
+    fab.env.user = SERVER_USER
+    fab.env.parallel = True
 
 def prepare_deploy(branch_name):
-    with settings(warn_only=True):
-        local('python manage.py schemamigration sds --auto')
-        local('python manage.py migrate sds')
-	local("git add -p --all :/ && git commit -a")
-        local('git checkout master && git merge ' + branch_name)
+    with fab.settings(warn_only=True):
+        fab.local('python manage.py schemamigration sds --auto')
+        fab.local('python manage.py migrate sds')
+	fab.local("git add -p --all :/ && git commit -a")
+        fab.local('git checkout master && git merge ' + branch_name)
 
 def deploy():
-    with settings(warn_only=True):
+    with fab.settings(warn_only=True):
         with cd('/home/ec2-user/sds'):
-            local('git push')
-            run('git pull')
-            run('python manage.py schemamigration sds --auto')
-            run('python manage.py migrate sds')
-            run('sudo /etc/init.d/httpd restart')
+            fab.local('git push')
+            fab.run('git pull')
+            fab.run('python manage.py schemamigration sds --auto')
+            fab.run('python manage.py migrate sds')
+            fab.run('sudo /etc/init.d/httpd restart')
 
 def reset_migrations():
     #navigate into the production database and run:  'drop table south_migrationhistory;'
