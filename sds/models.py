b269017c1fd2d18b478d4c04d5b6f85ccab30e5a
martin <martin.weiss@mail.mcgill.ca>
2015-01-18 17:42:38 +0000

    trying to make newsletter signup functionality work

--- sds/models.py
+++ sds/models.py
@@ -90,7 +90,7 @@ class UserProfile(models.Model):
     dancefloorSuperpower = models.CharField(max_length=2048)
     zipcode = models.CharField(max_length=10, default=00000, blank=True, null=True)
     discosAttended = models.PositiveSmallIntegerField(default=0)
-    newsletter = models.BooleanField(default=True)
+    newsletter = models.BooleanField(label='YEA!', required=True, initial=True)
     def __unicode__(self):
         return self.user.username
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
