d2eaa8b72b585fd31db48de2e08b09d051368940
martin <martin.weiss@mail.mcgill.ca>
2015-01-19 16:31:52 +0000

    added some forgotten JS

---
+++ static/js/recordEventAttendees.js
@@ -0,0 +1,23 @@
+
+var xmlhttp = new XMLHttpRequest();
+function record(){
+	//Hit the server's view that checks whether this account has already "attended" this event
+	try{
+		console.log("heyo");
+		xmlhttp.open("GET","/recordEventAttendees.html/?eventID="+eventID,true);
+		xmlhttp.send();
+	}
+	catch(err){
+		console.log( err );
+	}
+}
+
+function recordStream() {
+	record();
+	//window.location.href = "/stream.html?id="+eventID;
+}
+
+function recordDownload() {
+	record();
+	//window.location.href = "https://s3.amazonaws.com/silentdiscosquad/"+eventMix;
+}
