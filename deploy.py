67c573df83d6e22371a13da59da03ae986aa41d6
martin <martin.weiss@mail.mcgill.ca>
2015-01-26 19:49:14 +0000

    creates group then waits 5 minutes

--- deploy.py
+++ deploy.py
@@ -3,35 +3,42 @@ from boto.ec2.autoscale import AutoScaleConnection
 from boto.ec2.autoscale import LaunchConfiguration
 from boto.ec2.autoscale import AutoScalingGroup
 
-conn = boto.connect_ec2()
+conn = AutoScaleConnection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
 print conn.get_all_groups()
 old_autoscaling_group = conn.get_all_groups()
 old_config_group = conn.get_all_groups()
+create_image()
+create_autoscaling_conf()
+create_autoscaling_group()
 
 #Create an Image of the current server
-instance_id = boto.utils.get_instance_metadata()['instance-id']
-timestamp = time.time()
-value = datetime.datetime.fromtimestamp(timestamp)
-humanreadabledate = value.strftime('%Y-%m-%d_%H.%M.%S')
-image_name = 'productionImage'+humanreadabledate
-img = conn.create_image(instance_id, name, description=None, no_reboot=False, block_device_mapping=None, dry_run=False)
+def create_image():
+    instance_id = boto.utils.get_instance_metadata()['instance-id']
+    timestamp = time.time()
+    value = datetime.datetime.fromtimestamp(timestamp)
+    humanreadabledate = value.strftime('%Y-%m-%d_%H.%M.%S')
+    image_name = 'productionImage'+humanreadabledate
+    conn = boto.connect_ec2()
+    img = conn.create_image(instance_id, name, description=None, no_reboot=False, block_device_mapping=None, dry_run=False)
 
 #Create a new Autoscaling Configuration
-conn = AutoScaleConnection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
-autoscale = boto.ec2.autoscale.connect_to_region('us-east-1')
-print conn.get_all_groups()
-timestamp = time.time()
-value = datetime.datetime.fromtimestamp(timestamp)
-humanreadabledate = value.strftime('%Y-%m-%d_%H.%M.%S')
-config_name = 'live_launch_config'+humanreadabledate
-lc = LaunchConfiguration(name=config_name, image_id=img,
+def create_autoscaling_conf():
+    conn = AutoScaleConnection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+    autoscale = boto.ec2.autoscale.connect_to_region('us-east-1')
+    print conn.get_all_groups()
+    timestamp = time.time()
+    value = datetime.datetime.fromtimestamp(timestamp)
+    humanreadabledate = value.strftime('%Y-%m-%d_%H.%M.%S')
+    config_name = 'live_launch_config'+humanreadabledate
+    lc = LaunchConfiguration(name=config_name, image_id=img,
                              key_name='SDSEastKey',
                              security_groups=['sg-a7afb1c2'])
-conn.create_launch_configuration(lc)
+    conn.create_launch_configuration(lc)
 
 #Create new Autoscaling Group
-ag = AutoScalingGroup(group_name=config_name, load_balancers=['SDSLiveLoadBalancer'], availability_zones=['us-east-1a'], launch_config=lc, min_size=2, max_size=2, connection=conn)
-conn.create_auto_scaling_group(ag)
+def create_autoscaling_group():
+    ag = AutoScalingGroup(group_name=config_name, load_balancers=['SDSLiveLoadBalancer'], availability_zones=['us-east-1a'], launch_config=lc, min_size=2, max_size=2, connection=conn)
+    conn.create_auto_scaling_group(ag)
 
 
 print "Wait 5 minutes"
