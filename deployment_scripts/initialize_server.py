cde3dc8b42decd281f2da7322898fc34a712b19c
martin <martin.weiss@mail.mcgill.ca>
2015-02-02 19:32:09 +0000

    new live db

--- deployment_scripts/initialize_server.py
+++ deployment_scripts/initialize_server.py
@@ -1,3 +1,3 @@
 #!/bin/sh
 filename=/home/ec2-user/sds/sds/config.json 
-sed -i 's/preprod.cdadlb7rfieo.us-east-1.rds.amazonaws.com/sdslivejan28.cdadlb7rfieo.us-east-1.rds.amazonaws.com/g' $filename
+sed -i 's/preprod.cdadlb7rfieo.us-east-1.rds.amazonaws.com/sdslivefeb2.cdadlb7rfieo.us-east-1.rds.amazonaws.com/g' $filename
