70daba553fada208e45d1c8c44bb7cdcfbb83e4b
Ubuntu <ubuntu@ip-172-31-55-27.ec2.internal>
2015-06-09 17:13:07 +0000

    setting up angular

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 'use strict';
 
 // Declare variables used
-var ecstaticSockets, app, express, path, http;
+var ecstaticSockets, app, express, path, http, request, events;
 express = require('express');
 path = require('path');
 app = express();
 http = require('http');
+request = require('request');
 
 ecstaticSockets = require("./ecstaticSockets.js");
 ecstaticSockets.setupEcstaticSockets(app);
@@ -16,11 +17,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', "jade"); // your engine, you can use html, jade, ejs, vash, etc
 app.set('port', process.env.PORT || 80); // set up the port to be production or 80.
 app.use(express.static('static'));
-//app.set('env', process.env.NODE_ENV || 'development'); 
+
+//SERVER STARTUP SHIT
+//initialize event variable on server startup
+request('http://54.173.157.204/appindex/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    events = body;  
+  }
+});
+
+
+
 
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+	console.log(events);
+  	res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 //app.get('/', function(req, res) {
