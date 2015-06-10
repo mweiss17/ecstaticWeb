70de59fe439c53fb978a523abc4a2818495dcff4
Ubuntu <martin.clyde.weiss@gmail.com>
2015-06-10 18:57:23 +0000

    working on videogular

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,11 @@
-var app = angular.module('ecstatic', []);
-
-app.controller('mainController',['$scope', '$http', function($scope, $http) {
-//    $scope.test = "testytest";
-    // when landing on the page, get all todos and show them
+var app = angular.module('ecstatic', 
+	[        
+	    //"ngSanitize",
+		//"com.2fdevs.videogular",
+        //"com.2fdevs.videogular.plugins.controls",
+	]
+)
+.controller('mainController',['$scope', '$http', function($scope, $http) {
     $http.get('/api/upcomingEvents')
         .success(function(data) {
             $scope.upcomingEvents = data;
@@ -13,4 +16,17 @@ app.controller('mainController',['$scope', '$http', function($scope, $http) {
         .error(function(data) {
             console.log('Error: ' + data);
         });
-}]);
+}])
+.controller('HomeCtrl',
+        ["$scope", function($scope) {
+            this.config = {
+                sources: [
+              {src: $scope.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"), type: "audio/mpeg"},
+              {src: $scope.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
+          ],
+                theme: {
+          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                }
+            };
+        }]
+    );
